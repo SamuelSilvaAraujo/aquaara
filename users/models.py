f4d9a6138a0005872662fcc0ef92f763b495b915
samuels <samuelsas.samuel18@gmail.com>
2019-02-13 08:12:47 -0300

    melhorando User model

--- users/models.py
+++ users/models.py
@@ -1,13 +1,50 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.base_user import BaseUserManager, AbstractBaseUser
 from django.db import models
 
-class User(AbstractUser):
-    email = models.EmailField(unique=True)
-    name = models.CharField('Nome', max_length=40)
-    username = models.CharField(blank=True, null=True, max_length=150)
+class UserManager(BaseUserManager):
+    def create_user(self, email, name, password=None):
+        if not email:
+            raise ValueError('Os usuários devem ter um endereço de e-mail')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            name=name
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, name, password):
+        user = self.create_user(
+            email,
+            password=password,
+            name=name
+        )
+        user.is_admin = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractBaseUser):
+    email = models.EmailField("E-mail", unique=True)
+    name = models.CharField("Nome", max_length=40)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['name', 'username', ]
+    REQUIRED_FIELDS = ['name', ]
 
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return self.is_admin
