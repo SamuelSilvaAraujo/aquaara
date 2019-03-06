c8b44a09594a0b049275750e3c7e1053f98a4fc2
samuels <samuelsas.samuel18@gmail.com>
2019-03-06 11:03:51 -0300

    alterando url name e iniciando view pond detail

--- core/urls.py
+++ core/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('novapropriedade/', PropertyCreateView.as_view(), name="propriedade_create"),
     path('<slug:slug_property>/', include([
         path('viveiros/', PropertyPondsView.as_view(), name="propriedade_ponds"),
-        path('novoviveiro/', PondCreateView.as_view(), name="viveiro_create"),
-        path('viveiro/<slug:slug_pond>/', PondDetailView.as_view(), name="viveiro_detail"),
-        path('viveiro/<slug:slug_pond>/atualizar/',  PondUpdateView.as_view(), name="viveiro_update"),
-        path('viveiro/<slug:slug_pond>/excluir/',  PondDeleteView.as_view(), name="viveiro_delete"),
+        path('novoviveiro/', PondCreateView.as_view(), name="pond_create"),
+        path('viveiro/<slug:slug_pond>/', PondDetailView.as_view(), name="pond_detail"),
+        path('viveiro/<slug:slug_pond>/atualizar/',  PondUpdateView.as_view(), name="pond_update"),
+        path('viveiro/<slug:slug_pond>/excluir/',  PondDeleteView.as_view(), name="pond_delete"),
     ])),
 ]
